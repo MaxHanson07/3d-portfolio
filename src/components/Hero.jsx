4df634d2e1fb7eb87a69002c292c085248be2ef7
Max Hanson <mthanson2@gmail.com>
2023-09-12 12:25:55 -0700

    Started to build contact form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
       <div className='absolute x:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.dev animate={{
+            <motion.div animate={{
               y: [0, 24, 0]
             }}
             transition={{
