be2bdc8e718b3239ae48da3f009462ac1274d295
Max Hanson <mthanson2@gmail.com>
2023-09-12 14:38:30 -0700

    completed contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,8 +35,26 @@ const Contact = () => {
             border-none font-medium"/>
           </label>
 
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input type="email" name="email" value={form.email} onChange={handleChange} placeholder="What is your email?"
+            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none
+            border-none font-medium"/>
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea rows="7" name="message" value={form.message} onChange={handleChange} placeholder="What do you want to say?"
+            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none
+            border-none font-medium"/>
+          </label>
+
+          <button type="submit" className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold
+          shadow-md shadow-primary rounded-xl">
+            {loading ? 'Sending...' : 'Send'}</button>
         </form>
       </motion.div>
+
     </div>
   )
 }
