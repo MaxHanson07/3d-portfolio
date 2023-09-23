492b316b31b5607982caacd1b98b830ea7e20d70
Max Hanson <mthanson2@gmail.com>
2023-09-22 21:06:33 -0700

    reworked project section to be better looking

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,30 +19,47 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-<motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-  <div className="flex md:flex-row flex-col w-full">
-    <div className="sm:mb-5 relative">
-      <div className=" p-5 rounded-2xl sm:w-[480px] bg-[#8989890f] w-full md:mr-4">
-        <div className="mt-8">
-          <h3 className="text-[#ffffff] text-[20px] pb-5">{name}</h3>
-          <em className="text-white text-[28px] font-bold pb-5 leading-[30px]">
-            {tagline}
-          </em>
-          <p className="text-[#ffffff] pt-5 text-[18px] leading-[30px]">
-            {context}
-          </p>
-        </div>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <div className="mt-8">
+      <div className="flex items-center space-x-2">
+  <h3 className="text-white text-[28px] font-bold pb-5 pr-5 leading-[30px]">
+    {name}
+  </h3>
+  <div className="card-img_hover mb-5">
+    <div
+      onClick={() => window.open(source_code_link, "_blank")}
+      className=" w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+    >
+      <img
+        src={github}
+        alt="source code"
+        className="w-full h-full object-contain"
+      />
+    </div>
+  </div>
+</div>
 
-        <div>
-          {bullets && (
-            <ul className="list-disc ml-5 mt-2 text-white font-bold text-[18px] leading-[30px]">
-              {bullets.map((bullet, index) => (
-                <li key={index}>{bullet}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className="mt-4 flex flex-wrap gap-2">
+
+        <p className="text-[#ffffff] pt-5 pb-7 text-[18px] leading-[30px]">
+          {context}
+        </p>
+        <em className="text-white text-[28px] font-bold pb-5 leading-[30px]">
+          {tagline}
+        </em>
+      </div>
+      <div className="flex pt-10 md:flex-row flex-col w-full">
+        <div className="sm:mb-3 relative">
+          <div className=" p-5 rounded-2xl sm:w-[480px] w-full md:mr-4">
+            <div>
+              {bullets && (
+                <ul className="list-disc ml-5 mt-2 text-white font-bold text-[18px] leading-[30px]">
+                  {bullets.map((bullet, index) => (
+                    <li key={index}>{bullet}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <div className="mt-4 flex flex-wrap gap-2">
               {tags.map((tag) => (
                 <p
                   key={`${name}-${tag.name}`}
@@ -50,32 +67,21 @@ const ProjectCard = ({
                 >
                   #{tag.name}
                 </p>
-              ))} </div>
-      </div>
-      <div className="absolute top-2 right-4 m-2 card-img_hover">
-        <div
-          onClick={() => window.open(source_code_link, "_blank")}
-          className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-        >
+              ))}{" "}
+            </div>
+          </div>
+
+        </div>
+
+        <div className="w-full h-auto sm:mt-5 md:w-auto mb-5 md:mt-0 flex items-center justify-center rounded-2xl">
           <img
-            src={github}
-            alt="source code"
-            className="w-1/2 h-1/2 object-contain"
+            src={image}
+            alt="project_image"
+            className="w-full h-auto object-contain rounded-2xl"
           />
         </div>
       </div>
-    </div>
-
-    <div className="w-full h-auto sm:mt-5 md:w-auto mb-5 md:mt-0 flex items-center justify-center rounded-2xl">
-      <img
-        src={image}
-        alt="project_image"
-        className="w-full h-auto object-contain rounded-2xl"
-      />
-    </div>
-  </div>
-</motion.div>
-
+    </motion.div>
   );
 };
 
